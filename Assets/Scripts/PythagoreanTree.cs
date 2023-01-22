a9782fa96b24c202ac33dc05dcfa39061490ebbd
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2023-01-23 04:15:44 +0700

    feat: Added control sliders.

--- Assets/Scripts/PythagoreanTree.cs
+++ Assets/Scripts/PythagoreanTree.cs
@@ -1,27 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PythagoreanTree : MonoBehaviour
 {
     public float size;
     public float angle;
     public int iteration;
+    public int maxIteration;
 
     private Material standardMaterial;
     private Vector3 localScale1, localScale2;
     private Quaternion localRotation1, localRotation2;
     private Vector3 localPosition1, localPosition2;
 
+    private GameObject rootCube;
+    private List<GameObject> nonRootCubeList;
+
     void Awake()
     {
+        Assert.IsTrue(iteration <= maxIteration);
         standardMaterial = new Material(Shader.Find("Unlit/Color"));
         UpdateLocalTransformValue();
+        nonRootCubeList = new List<GameObject>();
     }
 
     void Start()
     {
-        GameObject cubeInstance = CubeGenerator.Generate(1, standardMaterial);
-        cubeInstance.transform.localScale = new Vector3(size, size, size);
-        Generate(iteration, cubeInstance.transform);
+        rootCube = CubeGenerator.Generate(1, standardMaterial);
+        rootCube.transform.localScale = new Vector3(size, size, size);
+        Generate(maxIteration, rootCube.transform);
     }
 
     void UpdateLocalTransformValue()
@@ -47,6 +56,8 @@ public class PythagoreanTree : MonoBehaviour
         {
             GameObject cubeInstance1 = CubeGenerator.Generate(1, standardMaterial);
             GameObject cubeInstance2 = CubeGenerator.Generate(1, standardMaterial);
+            nonRootCubeList.Add(cubeInstance1);
+            nonRootCubeList.Add(cubeInstance2);
 
             cubeInstance1.transform.SetParent(parent);
             cubeInstance2.transform.SetParent(parent);
@@ -64,5 +75,51 @@ public class PythagoreanTree : MonoBehaviour
         cubeInstance.transform.localScale = localScale;
         cubeInstance.transform.localRotation = localRotation;
         cubeInstance.transform.localPosition = localPosition;
+    }
+
+    void UpdateNonRootCubeVisibility(int hiddenIteration, int iteration, ref int index)
+    {
+        if(iteration > 0)
+        {
+            if( maxIteration - iteration < hiddenIteration )
+            {
+                nonRootCubeList[index].SetActive(true);
+                nonRootCubeList[index+1].SetActive(true);
+            }
+            else
+            {
+                nonRootCubeList[index].SetActive(false);
+                nonRootCubeList[index+1].SetActive(false);
+            }
+
+            index = index + 2;
+
+            UpdateNonRootCubeVisibility(hiddenIteration, iteration-1, ref index);
+            UpdateNonRootCubeVisibility(hiddenIteration, iteration-1, ref index);
+        }
+    }
+
+    public void ChangeSize(float value)
+    {
+        size = value;
+        rootCube.transform.localScale = new Vector3(size, size, size);
+    }
+
+    public void ChangeAngle(float value)
+    {
+        angle = Mathf.Deg2Rad*value;
+        UpdateLocalTransformValue();
+        for(int i=0; i<nonRootCubeList.Count; i+=2)
+        {
+            SetCubeLocalTransform( nonRootCubeList[i], localScale1, localRotation1, localPosition1);
+            SetCubeLocalTransform( nonRootCubeList[i+1], localScale2, localRotation2, localPosition2);
+        }
+    }
+
+    public void ChangeIteration(float value)
+    {
+        iteration = (int)value;
+        int index = 0;
+        UpdateNonRootCubeVisibility(iteration, maxIteration, ref index);
     }
 }
